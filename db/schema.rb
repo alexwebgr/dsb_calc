dd4e534a1fa76298360e24bf22c7805c86a638ac
alexwebgr <websurfer992@gmail.com>
2021-04-13 10:24:53 +0300

    added merchants

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_04_13_071854) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "merchants", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "cif"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
---
+++ spec/models/merchant_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe Merchant, type: :model do
+  describe 'associations' do
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:cif) }
+  end
+end
